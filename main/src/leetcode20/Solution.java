1365c88bcef12e4cd0c669f08d73f844a4641d1d
huangxiquan <huangxiquan111@qq.com>
2020-05-11 00:07:56 +0800

    20.有效的括号https://leetcode-cn.com/problems/valid-parentheses/

---
+++ main/src/leetcode20/Solution.java
@@ -0,0 +1,45 @@
+package leetcode20;
+
+import java.util.Stack;
+
+/**
+ * Created by huangxiquan on 2020/5/10.
+ * 20.有效的括号https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i = 0 ; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if(c == ')' && top != '(') {
+                    return false;
+                }
+                if(c == ']' && top != '[') {
+                    return false;
+                }
+                if(c == '}' && top != '{') {
+                    return false;
+                }
+                stack.pop();
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Solution().isValid("]"));
+        System.out.println(new Solution().isValid("()[]{}"));
+        System.out.println(new Solution().isValid("(]"));
+        System.out.println(new Solution().isValid("([)]"));
+        System.out.println(new Solution().isValid("{[]}"));
+    }
+}
